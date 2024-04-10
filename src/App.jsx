0a15323d3962c278f94d336d102809220fb682ff
chetan-chauhan-1502 <chauhanc1204@gmail.com>
2024-04-10 10:38:32 +0530

    changes filews

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
-import Footer from './Components/Footer/Footer'
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Home />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
